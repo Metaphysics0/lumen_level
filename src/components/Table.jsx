66718d00f140629e7e00342dffb467d695e73b08
Ryan Roberts <47161696+Metaphysics0@users.noreply.github.com>
2021-05-14 18:46:55 +0300

    first commit

---
+++ src/components/Table.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Table = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Date/Time</th>
+          <th>Session Type</th>
+          <th>Lumen Level</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Section 1</td>
+          <td>Section 2</td>
+          <td>Section 3</td>
+        </tr>
+        <tr>
+          <td>Section 1</td>
+          <td>Section 2</td>
+          <td>Section 3</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
